578a2479032409ddcb4736a66cd41952cc201263
AndreyKrasavin <78152473+AndreyKrasavin@users.noreply.github.com>
2023-10-13 11:18:53 +0300

    Create Test2.java

---
+++ HW_Java_OOP_6/DIP/Test2.java
@@ -0,0 +1,14 @@
+package DIP;
+
+/**
+ * Объекту типа Car всё равно какой у него будет двигатель. Главное чтобы он
+ * был в зависимости от абстракции Engine.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Car car = new Car(new PetrolEngine());
+        car.start();
+        car = new Car(new DieselPetrol());
+        car.start();
+    }
+}
